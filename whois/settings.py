98e0965f02b83fa06d3d5b010278d21b14eba6b6
Norbert Szulc <norbert@not7cd.net>
2018-09-18 20:23:23 +0200

    Hotfix

--- whois/settings.py
+++ whois/settings.py
@@ -1,6 +1,6 @@
 # mikrtotik ip, or other reporting devices
 name = "at.hs3.pl"
-whitelist = ["192.168.88.1"]
+whitelist = ["10.14.4.1"]
 host = "0.0.0.0"
 user_flags = {1: "hidden", 2: "name_anonymous"}
 device_flags = {1: "hidden", 2: "new", 4: "infrastructure", 8: "esp", 16: "laptop"}
