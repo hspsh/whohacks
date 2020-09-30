b858204a3c4bd176d222d2dfc842e96c9ed978a7
LiquidLemon <liquidlemon@protonmail.com>
2020-09-30 17:02:42 +0200

    Update the config

--- whois/settings.py
+++ whois/settings.py
@@ -1,6 +1,6 @@
 # mikrtotik ip, or other reporting devices
 name = "at.hs3.pl"
-whitelist = ["10.14.4.1", "10.14.10.1"]
+whitelist = ["192.168.88.1"]
 host = "0.0.0.0"
 user_flags = {1: "hidden", 2: "name_anonymous"}
 device_flags = {1: "hidden", 2: "new", 4: "infrastructure", 8: "esp", 16: "laptop"}
@@ -8,6 +8,6 @@ device_flags = {1: "hidden", 2: "new", 4: "infrastructure", 8: "esp", 16: "lapto
 recent_time = {"minutes": 20}
 
 # production
-ip_mask = "10.14.10.1-255"
+ip_mask = "192.168.88.1-255"
 # TODO: better way for handling dev env
 # ip_mask = "127.0.0.1"
