9196211053756fa103f6a3d767c6c66a1b0fe462
Norbert Szulc <norbert@not7cd.net>
2021-02-27 23:16:42 +0100

    Normalize mac address

--- whois/worker.py
+++ whois/worker.py
@@ -18,7 +18,7 @@ print(" [*] Waiting for logs. To exit press CTRL+C")
 
 def callback(ch, method, properties, body):
     print(" [x] %r:%r" % (method.routing_key, body))
-    Device.update_or_create(mac_address=body.decode(), last_seen=datetime.now())
+    Device.update_or_create(mac_address=body.decode().upper(), last_seen=datetime.now())
 
 
 channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
