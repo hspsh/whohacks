669198ba897c42afe4779842110da5c8b8df9055
Norbert Szulc <norbert@not7cd.net>
2021-03-08 00:15:57 +0100

    Add OIDC login flow

--- whois/settings.py
+++ whois/settings.py
@@ -21,6 +21,14 @@ device_flags = {1: "hidden", 2: "new", 4: "infrastructure", 8: "esp", 16: "lapto
 
 recent_time = {"minutes": 20}
 
+oidc_enabled = True
+# OAuth settings
+SSO_CLIENT_ID = os.environ.get("OAUTH_CLIENT_ID")
+SSO_CLIENT_SECRET = os.environ.get("OAUTH_CLIENT_SECRET")
+SSO_AUTH_URL = os.environ.get("OAUTH_AUTH_URL")
+SSO_TOKEN_URL = os.environ.get("OAUTH_TOKEN_URL")
+SSO_USERINFO_URL = os.environ.get("OAUTH_USERINFO_URL")
+
 # production
 ip_mask = "192.168.88.1-255"
 # TODO: better way for handling dev env
