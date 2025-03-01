8e93d97725f1f42dd28f627bec3f00f97f39f4de
Eduard Kaverinskyi <eduard.kaverinskyi@gmail.com>
2025-03-01 14:09:59 +0100

    logger.py: make logging to files optional
    Signed-off-by: Eduard Kaverinskyi <eduard.kaverinskyi@gmail.com>

--- helpers/logger.py
+++ helpers/logger.py
@@ -3,6 +3,24 @@ import os
 
 loggers = {}
 
+logging_dir = str(os.environ.get("LOG_DIR", "logs"))
+is_log_to_file = bool(os.environ.get("LOG_TO_FILE", "false").lower() == "true")
+
+is_logging_to_file_configured = False
+
+
+def configure_logging_to_file():
+    config_value = os.environ.get("LOG_TO_FILE")
+    if is_log_to_file:
+        print(f"Logging to file is enabled" f"(LOG_TO_FILE is set to '{config_value}')")
+        if logging_dir:
+            print(f"Logging files directory: '{logging_dir}'")
+            if not os.path.exists(logging_dir):
+                os.makedirs(logging_dir, exist_ok=True)
+    else:
+        print(f"Logging to file is disabled" f"(LOG_TO_FILE is set to '{config_value}')")
+
+
 
 def get_loglevel(loglevel: str):
     match loglevel:
@@ -23,6 +41,11 @@ def get_loglevel(loglevel: str):
 
 
 def init_logger(name: str) -> logging.Logger:
+    global is_logging_to_file_configured
+    if not is_logging_to_file_configured:
+        configure_logging_to_file()
+        is_logging_to_file_configured = True
+
     if loggers.get(name):
         return loggers[name]
 
@@ -39,11 +62,12 @@ def init_logger(name: str) -> logging.Logger:
 
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(formatter)
-    file_handler = logging.FileHandler(f"{name}.log")
-    file_handler.setFormatter(formatter)
-
     logger.addHandler(stream_handler)
-    logger.addHandler(file_handler)
+
+    if is_log_to_file:
+        file_handler = logging.FileHandler(f"{logging_dir}/{name}.log")
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
 
     logger.setLevel(loglevel)
 
